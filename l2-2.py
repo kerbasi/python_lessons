abb3ed40291e8a014d40acd39d9658361ca20f89
Kirill <kirill.novikov@gmail.com>
2022-09-23 21:39:46 +0300

    Add solution for task 2 and add gitignore

---
+++ l2-2.py
@@ -0,0 +1,14 @@
+﻿import math
+from random import Random, random
+from xml.etree.ElementInclude import include
+
+
+import math
+#Найти максимальное из 5 чисел
+numbers_list = [3,5,6,3,7]
+for i in range(5):
+    numbers_list[i] = round(random() * 10)
+
+
+print((numbers_list))
+print(max(numbers_list))
