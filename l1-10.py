5d27d68f16f55e0f879431358770765679829d11
Kirill <kirill.novikov@gmial.com>
2022-09-26 08:34:12 +0300

    Add all tasks as lessons, delete all hw.

--- l1-10.py
+++ l1-10.py
@@ -6,4 +6,5 @@ x1 = int(input("x1: "))
 y1 = int(input("y1: "))
 x2 = int(input("x2: "))
 y2 = int(input("y2: "))
+
 print(f"Расстояние между точками равно: {math.sqrt( ((y2 - y1) ** 2) + ((x2 - x1) ** 2))}")
