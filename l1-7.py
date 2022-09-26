2b8a669fcb5d9f82d6337cffeb7e58912bba1159
Kirill <kirill.novikov@gmial.com>
2022-09-26 09:01:09 +0300

    Add solution for task 7 and minor changes for task 10

--- l1-7.py
+++ l1-7.py
@@ -1,3 +1,10 @@
 ﻿# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
 # для всех значений предикат.
 
+def logic_checker(x , y , z):
+    return not (x or y or z) == (not x and not y and not z)
+
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            print(f"Для значений x={i} y={j} z={k} выражение {'ИСТИННО' if logic_checker(i , j , k) else 'ЛОЖНО'}")
